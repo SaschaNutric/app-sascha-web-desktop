042c8e7f58eca97a728818ef040f5ee662852054
skarlyruiz <skarly.ruiz@gmail.com>
2018-05-17 22:05:28 -0430

    Configuración de filtros

--- public/js/conf_sist_filtros.js
+++ public/js/conf_sist_filtros.js
@@ -1,91 +1,129 @@
 /* tabla filtros */
 $(document).ready(function() {
       
-    $('#dtFiltros').dataTable({ 
+    /* tabla filtros */
+    const tablaFiltros = $('#dtFiltros').DataTable({ 
         "aoColumnDefs": [
         { "bSortable": false, "aTargets": [1] }
-        ],               
-        "sDom": "ftp",
-        "oLanguage": {
-            "sLengthMenu": "",
-            "sSearch": "Buscar:",
-            "oPaginate": {
-                "sPrevious": "Anterior",
-                "sNext": "Siguiente"
+        ],
+        "language": {
+            "lengthMenu": "",
+            "search": "Buscar:",
+            "paginate": {
+                "previous": "Anterior",
+                "next": "Siguiente"
             },
-            "SEmptyTble": "No se encontraron medidas"
-        },        
+            "emptyTable": "No se encontraron tipos de parametros",
+            "zeroRecords": "No se encontraron tipos de parametros"
+        },
+        "searching": true,
+        "ordering": true,
+        "paging": true
     });
 
-/*    $('#ms_parametros').multiSelect({
-    selectableHeader: "<input type='text' class='form-control search-input' autocomplete='off' placeholder='Buscar...'>",
-    selectionHeader: "<input type='text' class='form-control search-input' autocomplete='off' placeholder='Buscar...'>",
-    afterInit: function (ms) {
-        var that = this,
-            $selectableSearch = that.$selectableUl.prev(),
-            $selectionSearch = that.$selectionUl.prev(),
-            selectableSearchString = '#' + that.$container.attr('id') + ' .ms-elem-selectable:not(.ms-selected)',
-            selectionSearchString = '#' + that.$container.attr('id') + ' .ms-elem-selection.ms-selected';
+    $.ajax({
+        url: 'https://api-sascha.herokuapp.com/tipoparametros',
+        contentType: 'application/json',
+        type: 'GET',
+        success: function(res, status, xhr) {
+            res.data.map(function(tipoParametro) {        
+                addRowTipoParametro(tipoParametro.id_tipo_parametro,tipoParametro.nombre, tipoParametro.filtrable)
+            })
 
-        that.qs1 = $selectableSearch.quicksearch(selectableSearchString)
-            .on('keydown', function (e) {
-                if (e.which === 40) {
-                    that.$selectableUl.focus();
-                    return false;
-                }
-            });
+        },
+        error: function(res, status, xhr) {
+            console.log(res)
+            console.log(status)
+            const respuesta = JSON.parse(res.responseText);
+            mensaje('#msjAlerta', `${respuesta.data.mensaje}`, 0);
 
-        that.qs2 = $selectionSearch.quicksearch(selectionSearchString)
-            .on('keydown', function (e) {
-                if (e.which == 40) {
-                    that.$selectionUl.focus();
-                    return false;
-                }
-            });
-    },
-    afterSelect: function () {
-        this.qs1.cache();
-        this.qs2.cache();
-    },
-    afterDeselect: function () {
-        this.qs1.cache();
-        this.qs2.cache();
-    }
+        }
+    })
 });
 
-    $('#ms_parametros-editar').multiSelect({
-    selectableHeader: "<input type='text' class='form-control search-input' autocomplete='off' placeholder='Buscar...'>",
-    selectionHeader: "<input type='text' class='form-control search-input' autocomplete='off' placeholder='Buscar...'>",
-    afterInit: function (ms) {
-        var that = this,
-            $selectableSearch = that.$selectableUl.prev(),
-            $selectionSearch = that.$selectionUl.prev(),
-            selectableSearchString = '#' + that.$container.attr('id') + ' .ms-elem-selectable:not(.ms-selected)',
-            selectionSearchString = '#' + that.$container.attr('id') + ' .ms-elem-selection.ms-selected';
-
-        that.qs1 = $selectableSearch.quicksearch(selectableSearchString)
-            .on('keydown', function (e) {
-                if (e.which === 40) {
-                    that.$selectableUl.focus();
-                    return false;
-                }
-            });
-
-        that.qs2 = $selectionSearch.quicksearch(selectionSearchString)
-            .on('keydown', function (e) {
-                if (e.which == 40) {
-                    that.$selectionUl.focus();
-                    return false;
-                }
-            });
-    },
-    afterSelect: function () {
-        this.qs1.cache();
-        this.qs2.cache();
-    },
-    afterDeselect: function () {
-        this.qs1.cache();
-        this.qs2.cache();
+function addRowTipoParametro(id, nombre, filtrable){
+    let row;
+    if(filtrable == false){
+        row = $(`<tr>
+        <td id="nombre-${id}">${nombre}</td>
+        <td>
+            <button onclick="filtrable(${id})" type='button' class='edit btn  btn-aceptar' title='filtrable'>Filtrable</i></button>
+        </td>
+        </tr>
+        `);
+    }else{
+    row = $(`<tr>
+        <td id="nombre-${id}">${nombre}</td>
+        <td>
+            <button onclick="noFiltrable(${id})" type='button' class='ver btn  btn-cancelar' title='noFiltrable'>No Filtrable</i></button>
+        </td>
+        </tr>
+        `);
     }
-});*/
-});
+   $('#dtFiltros').DataTable().row.add(row).draw();
+}
+
+function filtrable(id){
+    let parametro = {
+        filtrable: true
+    }
+    $.ajax({
+        url: `https://api-sascha.herokuapp.com/tipoparametro/${id}/filtrable`,
+        contentType: 'application/json',
+        type: 'PUT',
+        data: JSON.stringify(parametro),
+        success: function(res, status, xhr) {
+            console.log(res.data)
+            let row = $(`<tr>
+            <td id="nombre-${res.data.id_tipo_parametro}">${res.data.nombre}</td>
+            <td>
+                <button onclick="noFiltrable(${res.data.id_tipo_parametro})" type='button' class='ver btn  btn-cancelar' title='noFiltrable'>No Filtrable</i></button>
+            </td>
+            </tr>
+            `);
+            $('#dtFiltros').DataTable().row($(`#nombre-${id}`).parent()).remove().draw();
+            $('#dtFiltros').DataTable().row.add(row).draw();
+            mensaje('#msjAlerta', `Tipo de parametro`, 8);
+        },
+        error: function(res, status, xhr) {
+            console.log(res);
+            console.log(status);
+            const respuesta = JSON.parse(res.responseText);
+            mensaje('#msjAlerta',`${respuesta.data.mensaje}`, 0);
+
+        }
+    })
+}
+
+function noFiltrable(id){
+    let parametro = {
+        filtrable: false
+    }
+    $.ajax({
+        url: `https://api-sascha.herokuapp.com/tipoparametro/${id}/filtrable`,
+        contentType: 'application/json',
+        type: 'PUT',
+        data: JSON.stringify(parametro),
+        success: function(res, status, xhr) {
+            console.log(res.data)
+            let row = $(`<tr>
+            <td id="nombre-${res.data.id_tipo_parametro}">${res.data.nombre}</td>
+            <td>
+                <button onclick="filtrable(${res.data.id_tipo_parametro})" type='button' class='edit btn  btn-aceptar' title='filtrable'>Filtrable</i></button>
+            </td>
+            </tr>
+            `);
+            $('#dtFiltros').DataTable().row($(`#nombre-${id}`).parent()).remove().draw();
+            $('#dtFiltros').DataTable().row.add(row).draw();
+            mensaje('#msjAlerta', `Tipo de parametro`, 9);
+        },
+        error: function(res, status, xhr) {
+            console.log(res);
+            console.log(status);
+            const respuesta = JSON.parse(res.responseText);
+            mensaje('#msjAlerta',`${respuesta.data.mensaje}`, 0);
+
+        }
+    })
+}
+
