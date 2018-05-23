c5efaf542701d4d2ae2a50a5dac7a973952f35ba
PedroAliOrellana <pedroaliorellana@gmail.com>
2018-05-23 15:26:07 -0400

    Solucion de conflictos

--- public/js/mensaje.js
+++ public/js/mensaje.js
@@ -25,30 +25,11 @@ function mensaje(id, entidad, accion){
         case 5: 
         tipo= "alert alert-danger";
         texto = `<strong>Error!</strong> Rellena los campos obligatorios.`;
-<<<<<<< HEAD
         case 10:
         tipo= "alert alert-info";
         texto = `<strong>${entidad}</strong>  No encontrado.`;
 
 
-=======
-        break;
-        case 6:
-        tipo = "alert alert-danger";
-        texto = `<strong>Error!</strong> Rangos inválidos.`;
-        break;
-        case 7:
-        tipo = "alert alert-info";
-        texto = `<strong>Info!</strong> Debes registrar el(la) ${entidad} primero.`;
-        break;
-        case 8:
-        tipo= "alert alert-success";
-        texto = `<strong>Éxito!</strong> ${entidad} configurado como filtro.`;
-        break;
-        case 9:
-        tipo= "alert alert-success";
-        texto = `<strong>Éxito!</strong> ${entidad} ha dejado ser filtro.`;
->>>>>>> 2116ed4ea389e6cc1c5ed8fe511cc3074e3d0449
 
     }
     $(id).removeClass();
