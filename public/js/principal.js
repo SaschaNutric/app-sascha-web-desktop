7b9f0a4cba7728f1da4ca03044afdcf2702855ca
SaschaNutric <saschanutric@gmail.com>
2018-05-28 17:56:02 -0400

    quitando la trampa de login

--- public/js/principal.js
+++ public/js/principal.js
@@ -1,10 +1,16 @@
 $(document).ready(function (event) {
+    
+    if(!localStorage.sesion) {
+        window.location ="index.html";
+    }
     let empleado = JSON.parse(localStorage.getItem('empleado'));
     $('span#username').text(empleado.nombres + ' ' + empleado.apellidos);
 
     $('#btnCerrarSesion').on('click', function(event) {
         localStorage.removeItem('empleado');
         localStorage.removeItem('token');
+        localStorage.removeItem('sesion');
+        localStorage.setItem('sesion',false);
         window.location = 'index.html'; 
     })
 })
