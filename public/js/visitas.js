6b70578b74f5ce05f22c3e0615ea45c79a1507eb
SaschaNutric <saschanutric@gmail.com>
2018-05-28 02:44:37 -0400

    visita validada y camibios en roles

--- public/js/visitas.js
+++ public/js/visitas.js
@@ -1,5 +1,21 @@
 var Script = function () {
 
+    $('#dtDetalle').DataTable({
+        "language": {
+            "lengthMenu": "",
+            "search": "Buscar:",
+            "paginate": {
+                "previous": "Anterior",
+                "next": "Siguiente"
+            },
+            "emptyTable": "No se encontraron detalles",
+            "zeroRecords": "No se encontraron detalles"
+        },
+        "pageLength": 5,
+        "searching": true,
+        "ordering": true,
+        "paging": true
+    });
     /* initialize the calendar
     -----------------------------------------------------------------*/
     let data = {
@@ -21,6 +37,42 @@ var Script = function () {
         defaultView : 'basicDay',
         editable: false,
         droppable: false, // this allows things to be dropped onto the calendar !!!
+        eventClick: function(event, jsEvent, view){
+            if(event.id_visita == null){
+                window.location = event.url_agenda
+            }else{
+                let agenda =event.agenda
+                let fecha_cita = moment(event.start).format('DD, MMMM YYYY')
+                $('#cliente-nombre').text(agenda.nombre_cliente)
+                $('#servicio-nombre').text(agenda.nombre_servicio)
+                
+                $('#cita-fecha').text(fecha_cita)
+                $('#cita-tipo').text(agenda.tipo_cita)
+
+                if (agenda.id_tipo_cita == 2) {
+                    if ($('#color-icono-visita').hasClass('light-green')) {
+                        $('#color-icono-visita').removeClass('light-green')
+                    }
+                    if ($('#icono-visita').hasClass('fa-stethoscope')) {
+                        $('#icono-visita').removeClass('fa-stethoscope')
+                    }
+                    $('#color-icono-visita').addClass('turquoise')
+                    $('#icono-visita').addClass('fa-eye')
+                }else{
+                    if ($('#color-icono-visita').hasClass('turquoise')) {
+                        $('#color-icono-visita').removeClass('turquoise')
+                    }
+                    if ($('#icono-visita').hasClass('fa-eye')) {
+                        $('#icono-visita').removeClass('fa-eye')
+                    }
+                    $('#color-icono-visita').addClass('light-green')
+                    $('#icono-visita').addClass('fa-stethoscope')
+                }
+                cargarDetalle(event.id_visita)
+                
+                $('#modalDetalle').modal()
+            }
+        }
         
     });
 
@@ -36,10 +88,12 @@ var Script = function () {
 
          let event = {
             title: agenda.horario + " - " + agenda.nombre_cliente,
+            id_visita: agenda.id_visita,
+            agenda: agenda,
             start: agenda.fecha_inicio,
             end: agenda.fecha_fin,
             color: (agenda.id_tipo_cita ==1 ? '#7ab740': '#3da3cb'),
-            url: `visi_registrarVisita.html?id=${agenda.id_agenda}`
+            url_agenda: `visi_registrarVisita.html?id=${agenda.id_agenda}`
         }
         events.push(event);
     })
@@ -55,3 +109,30 @@ var Script = function () {
 
 
 }();
+
+function cargarDetalle(id){
+    $('#dtDetalle').DataTable().clear()
+    $.ajax({
+        url: 'https://api-sascha.herokuapp.com/detalles/visita/'+id,
+        contentType: 'application/json',
+        type: 'GET',
+        success: function (res, status, xhr) {
+            res.data.detalles.map(function (detalle) {
+                let valor = detalle.valor == null ? '-': detalle.valor + " " + detalle.unidad_abreviatura
+                let row = $(`<tr>   
+                <td>${detalle.tipo_parametro}</td>
+                <td>${detalle.nombre}</td>
+                <td>${valor}</td>
+                </tr>
+                `);
+            $('#dtDetalle').DataTable().row.add(row).draw();
+            })
+
+        },
+        error: function (res, status, xhr) {
+            const respuesta = JSON.parse(res.responseText);
+            mensaje('#msjAlerta', `${respuesta.data.mensaje}`, 0);
+
+        }
+    })
+}
