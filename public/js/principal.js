48548e093093c8c0c94467657bca4ea899f8ef47
SaschaNutric <saschanutric@gmail.com>
2018-05-28 21:59:14 -0400

    arrreglando el put de roles

--- public/js/principal.js
+++ public/js/principal.js
@@ -1,6 +1,22 @@
 $(document).ready(function (event) {
     let empleado = JSON.parse(localStorage.getItem('empleado'));
+
+    let usuario = empleado.usuario
+    let menu = usuario.rol.funcionalidades
+    console.log(menu)
+    console.log(usuario)
+
+    if(!usuario.rol || usuario.rol == null){
+        alert("no tienes acceso a la intranet")
+        window.location.replace('index.htmtl')
+    }
+    
     $('span#username').text(empleado.nombres + ' ' + empleado.apellidos);
+
+
+
+
+
 
     $('#btnCerrarSesion').on('click', function(event) {
         localStorage.removeItem('empleado');
