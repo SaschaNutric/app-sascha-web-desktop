bec7410e5c7b57928d1c1c042677a153179e280b
Jose7sil <silvas89k@gmail.com>
2018-06-05 05:53:14 -0430

    editado mensaje.js--comu_canal_escucha.js--comu_canal_escucha.html

--- public/js/comu_canal_escucha.js
+++ public/js/comu_canal_escucha.js
@@ -52,56 +52,20 @@ $(document).ready(function () {
     }
   });
 
-  /* url: 'https://api-sascha.herokuapp.com/comentarios',
-   contentType: 'application/json',
-   type: 'GET',
-   success: function(res, status, xhr) {
-   console.log(res.data);
-   res.data.map(function(comentario) { 
-     let respuesta = '';
-     let mensaje = comentario.mensaje || '';
-     if (comentario.id_respuesta != null) respuesta = comentario.respuesta.descripcion;
-
-             let addRowTipoComentario  let row = $(`<tr>
-                 <td id="fecha_creacion-${comentario.id}">${moment('comentario.fecha_creacion','YYYY-MM-DD, h:mm:ss a').format('DD-MM-YYYY, h:mm:ss a')}</td>
-                 <td id="cliente-${comentario.id}">${comentario.cliente}</td>
-                 <td id="tipo_motivo-${comentario.id}">${comentario.tipo_motivo}</td>
-                 <td id="motivo-${comentario.id}">${comentario.motivo}</td>
-                 <td id="contenido-${comentario.id}">${comentario.contenido}</td>
-                 <td>
-                   <button onclick="editarComunicacion(${comentario.id}, ${comentario.id_tipo_motivo})" type='button' class='edit btn  btn-transparente' data-toggle="modal" data-target="#modal-enviar-respuesta"  title='Editar'>
-                   <i class='fa fa-pencil'></i>
-                   </button>
-                 </td>
-             </tr>
-   `);
-   $('#dt2canalescucha').DataTable().row.add(row).draw()
-           })
-
-       },
-       error: function (res, status, xhr) {
-           const respuesta = JSON.parse(res.responseText);
-           mensaje('#msjAlerta', `${respuesta.data.mensaje}`, 0);
-       }
-   })
-
-*/
-
-
-
   $('#btnEnviar').on('click', function () {
-    if ($('select[name=selRespuesta]').val() == "0") {
+    if ($('#selRespuesta').val() === "0") {
+      console.log("POLICE")
+      console.log($('select[name=selRespuesta]').val());
       $('select[name=selRespuesta]').css('border', '1px solid red')
+        mensaje('#msjAlertaModal', ``, 5);
+
       return;
     }
-
     let comentario = {
       id_respuesta: $('select[name=selRespuesta]').val(),
       mensaje: $('#txtMensaje').val()
     }
-
     const descripcion_respuesta = $('select[name="selRespuesta"] option:selected').text()
-
     let id = $('#txtIdComunicacion').val();
 
 
@@ -111,7 +75,7 @@ $(document).ready(function () {
       type: 'PUT',
       data: JSON.stringify(comentario),
       success: function (res, status, xhr) {
-        mensaje('#msjAlerta', `Motivo`, 3);
+        mensaje('#msjAlerta', `Exito`, 16);
         $('#dt2canalescucha').DataTable().row($(`#tipo_motivo-${id}`).parent()).remove().draw()
         limpiar();
         $('#modal-enviar-respuesta').modal('hide')
@@ -163,22 +127,35 @@ function editarComunicacion(id, id_tipo_motivo) {
   $('#txtIdComunicacion').val(id);
   $('#selRespuesta option:contains(' + $(`#respuesta-${id}`).text() + ')').prop('selected', true);
   $('#txtCliente').val($(`#cliente-${id}`).text());
+  $('#txtMotivo').val($(`#motivo-${id}`).text());
   $('#txtComentario').val($(`#contenido-${id}`).text());
   $('#txtMensaje').val($(`#mensaje-${id}`).text());
   $('#btnCancelar').css('display', 'none');
   $('#btnEnviar').css('display', 'inline');
 }
-
 function refressRowTipoComentario(id, respuesta, mensaje) {
   $(`#respuesta-${id}`).text(respuesta)
   $(`#mensaje-${id}`).text(mensaje)
 }
-
 function limpiar() {
   $('#txtIdComunicacion').val('');
   $('#selRespuesta option:contains(Seleccione)').prop('selected', true);
   $('#txtCliente').val('');
+  $('#txtMotivo').val('');
   $('#txtComentario').val('');
   $('#txtMensaje').val('');
+}
+function validate() {
+    let validate = true;
+    let ofertaRespuesta = {
+        nombre: $('#txtCliente').val(),
+        servicio: $('#txtMotivo').val(),
+        descripcion: $('#txtComentario').val(),
+        descuento: $('#txtMensaje').val(),
+    }
+    if (ofertaPromo.nombre == '' || ofertaPromo.servicio == 0 || ofertaPromo.descripcion == '' || ofertaPromo.descuento == '' || ofertaPromo.valido_desde == '' || ofertaPromo.valido_hasta == '' || ofertaPromo.estado_civil == 0 || ofertaPromo.rango_edad == 0 || ofertaPromo.genero == 0 ) {
+        validate = false;
+    }
 
+    return validate;
 }
